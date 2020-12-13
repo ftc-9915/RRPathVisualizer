23fe72e7ba807dd4cf00ed53dd15b369f98e6401
Trevor Kwan <trevorkw7@gmail.com>
2020-12-13 01:27:59 -0800

    Drafted a Blue 1 Four Ring Path in TrajectoryGen.kt

--- src/main/kotlin/TrajectoryGen.kt
+++ src/main/kotlin/TrajectoryGen.kt
@@ -14,23 +14,85 @@ object TrajectoryGen {
 
     private val combinedConstraints = MecanumConstraints(driveConstraints, trackWidth)
 
-    private val startPose = Pose2d(-48.0, -48.0, 90.0.toRadians)
+    private val startPoseBlue1 = Pose2d(-63.0, 48.0, 0.0.toRadians)
+    private val startPoseBlue2 = Pose2d(-48.0, -48.0, 90.0.toRadians)
+
 
     fun createTrajectory(): ArrayList<Trajectory> {
-        val list = ArrayList<Trajectory>()
+        var list = ArrayList<Trajectory>()
 
-        val builder1 = TrajectoryBuilder(startPose, startPose.heading, combinedConstraints)
 
-        builder1.forward(40.0);
+        //Declare Routes
+        val blue1_RingFourRoute = ArrayList<Trajectory>();
+        val blue1_RingOneRoute  = ArrayList<Trajectory>();
+        val blue1_RingNoneRoute = ArrayList<Trajectory>();
 
-        // Small Example Routine
-//        builder1
-//            .splineTo(Vector2d(10.0, 10.0), 0.0)
-//            .splineTo(Vector2d(15.0, 15.0), 90.0);
 
-        list.add(builder1.build())
+        // -- Prose and Trajectory Library --
 
-        return list
+        val shootPoseBlue1 = Pose2d(0.0, 48.0, 0.0.toRadians)
+        val shootPoseBlue1PowerShot1 = Pose2d(0.0, 48.0, -4.0.toRadians)
+        val shootPoseBlue1PowerShot2 = Pose2d(0.0, 48.0, -8.0.toRadians)
+        val shootPoseBlue1PowerShot3 = Pose2d(0.0, 48.0, -12.0.toRadians)
+        val zoneCPoseBlue = Pose2d(48.0, 48.0, 20.0.toRadians)
+        val ringPoseMiddleBlue1 = Pose2d(0.0, 40.0, 180.0.toRadians)
+        val ringPoseBlue1 = Pose2d(-24.0, 36.0, 180.0.toRadians)
+        val wobbleGoalBlue1 = Pose2d(-37.0, 32.0, 190.0.toRadians)
+        val parkingPositionBlue1 = Pose2d(12.0, 42.0, 20.0.toRadians)
+
+        //Version with power shot
+        val blue1ToShootPowerShot = TrajectoryBuilder(startPoseBlue1, startPoseBlue1.heading, combinedConstraints)
+        blue1ToShootPowerShot
+            .splineTo(shootPoseBlue1PowerShot1.vec(), shootPoseBlue1PowerShot1.heading)
+
+        val blue1PowerShotToZoneC = TrajectoryBuilder(shootPoseBlue1PowerShot3, shootPoseBlue1PowerShot3.heading, combinedConstraints)
+        blue1PowerShotToZoneC
+            .lineToSplineHeading(zoneCPoseBlue)
+
+        //Version with high goal
+        val blue1ToShootHighGoal = TrajectoryBuilder(startPoseBlue1, startPoseBlue1.heading, combinedConstraints)
+        blue1ToShootHighGoal
+            .splineTo(shootPoseBlue1.vec(), shootPoseBlue1.heading)
+
+        val blue1HighGoalToZoneC = TrajectoryBuilder(shootPoseBlue1, shootPoseBlue1.heading, combinedConstraints)
+        blue1HighGoalToZoneC
+            .lineToSplineHeading(zoneCPoseBlue)
+
+        val blue1ZoneCToRingWobbleGoal = TrajectoryBuilder(zoneCPoseBlue, zoneCPoseBlue.heading, combinedConstraints)
+        blue1ZoneCToRingWobbleGoal
+            .lineToSplineHeading(ringPoseMiddleBlue1)
+            .lineToConstantHeading(ringPoseBlue1.vec())
+            .splineTo(wobbleGoalBlue1.vec(), wobbleGoalBlue1.heading)
+
+        val blue1WobbleGoalToShoot = TrajectoryBuilder(wobbleGoalBlue1, wobbleGoalBlue1.heading, combinedConstraints)
+        blue1WobbleGoalToShoot
+            .lineToSplineHeading(shootPoseBlue1)
+
+        val blue1ShootToZoneC = TrajectoryBuilder(shootPoseBlue1, shootPoseBlue1.heading, combinedConstraints)
+        blue1ShootToZoneC
+            .lineToSplineHeading(zoneCPoseBlue)
+
+        val blue1ZoneCToPark = TrajectoryBuilder(zoneCPoseBlue, zoneCPoseBlue.heading, combinedConstraints)
+        blue1ZoneCToPark
+            .lineToSplineHeading(parkingPositionBlue1)
+
+
+        //Version with power shot
+//        blue1_RingFourRoute.add(blue1ToShootPowerShot.build())
+//        blue1_RingFourRoute.add(blue1PowerShotToZoneC.build())
+
+        //Version with high goal
+        blue1_RingFourRoute.add(blue1ToShootHighGoal.build())
+        blue1_RingFourRoute.add(blue1HighGoalToZoneC.build())
+
+        blue1_RingFourRoute.add(blue1ZoneCToRingWobbleGoal.build())
+        blue1_RingFourRoute.add(blue1WobbleGoalToShoot.build())
+        blue1_RingFourRoute.add(blue1ShootToZoneC.build())
+        blue1_RingFourRoute.add(blue1ZoneCToPark.build())
+
+
+
+        return blue1_RingFourRoute
     }
 
     fun drawOffbounds() {
@@ -39,3 +101,4 @@ object TrajectoryGen {
 }
 
 val Double.toRadians get() = (Math.toRadians(this))
+
